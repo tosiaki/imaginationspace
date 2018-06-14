5d449887ef478f246ddc21dd49c8f1b57b847f08
Tosiaki <aworldwithoutsnowflakes1@gmail.com>
2018-06-14 10:33:59 -0400

    Adding sendgrid config

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    get 'signup', to: 'users/registrations#new', as: :registration
+    post 'signup', to: 'users/registrations#create'
+  end
+
   devise_for :users
   get 'pages/home'
   root 'pages#home'
