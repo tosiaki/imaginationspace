8ca927d4ea241f29d88f67cc8e178090da7ae60c
Tosiaki <aworldwithoutsnowflakes1@gmail.com>
2018-06-26 11:33:18 -0400

    Fixing interface for front page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,8 @@ class PagesController < ApplicationController
   def home
     @comics = Comic.all.paginate(page: 1, per_page: 100)
     @drawings = Drawing.all.paginate(page: 1, per_page: 100)
-    @fandoms = @drawings.tag_counts_on(:fandoms, order: "count desc")
+    @fandoms_drawings = @drawings.tag_counts_on(:fandoms, order: "count desc")
+    @fandoms_comics = @comics.tag_counts_on(:fandoms, order: "count desc")
 
     if user_signed_in?
       @feed_drawings = current_user.feed_drawings
