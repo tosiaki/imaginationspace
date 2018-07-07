7ceb345c85beb19c9518e6960e3660f86250f31b
Tosiaki <aworldwithoutsnowflakes1@gmail.com>
2018-07-06 21:33:41 -0400

    Changing the count to be specific to model

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,8 @@ class PagesController < ApplicationController
   def home
     @comics = Comic.all.paginate(page: 1, per_page: 100)
     @drawings = Drawing.all.paginate(page: 1, per_page: 100)
-    @fandoms_drawings = @drawings.tag_counts_on(:fandoms, order: "count desc")
-    @fandoms_comics = @comics.tag_counts_on(:fandoms, order: "count desc")
+    @fandoms_drawings = specific_count('Drawing')
+    @fandoms_comics = specific_count('Comic')
 
     if user_signed_in?
       @feed_drawings = current_user.feed_drawings
@@ -13,4 +13,9 @@ class PagesController < ApplicationController
 
   def about
   end
+
+  private
+    def specific_count(model)
+      ActsAsTaggableOn::Tag.select('tags.id, tags.name, COUNT(tags.id) as count').joins(:taggings).where( taggings: { context: 'fandoms', taggable_type: model } ).group('tags.id').order('count DESC')
+    end
 end
