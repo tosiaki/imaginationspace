151388486ac9271f16fd6f8cefa261c21509698d
Tosiaki <aworldwithoutsnowflakes1@gmail.com>
2018-08-27 23:20:16 -0400

    Adding test for comic page display and removing an unecessary .id

--- spec/features/user_visits_a_work_spec.rb
+++ spec/features/user_visits_a_work_spec.rb
@@ -5,4 +5,30 @@ RSpec.feature "User visits a work", :type => :feature do
     # The comments counter should not display
     # it will be displayed if using the @work.comments.any? method due to the unsaved comment
   end
+
+  describe "the actions links" do
+    let!(:comic) { create(:comic) }
+
+    context "when it is not the user's own work" do
+      background do
+        visit comic_path(comic)
+      end
+
+      it 'does not contain a link to add a page' do
+        expect(page.body).to_not have_link("Add page(s)", href: new_comic_page_path(comic))
+      end
+    end
+
+    context "when it is the user's own work" do
+      background do
+        user = comic.user
+        sign_in user
+        visit comic_path(comic)
+      end
+
+      it 'contains a link to add a page' do
+        expect(page.body).to have_link("Add page(s)", href: new_comic_page_path(comic))
+      end
+    end
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
@@ -57,5 +56,6 @@ RSpec.configure do |config|
 
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
+  config.include Devise::Test::IntegrationHelpers, type: :feature
   config.include Warden::Test::Helpers
 end
