ed69122a83e4fc043845fdefffbabd95cc4b4ccc
Tosiaki <aworldwithoutsnowflakes1@gmail.com>
2018-08-27 20:10:39 -0400

    Fixing pending test

--- spec/models/kudo_spec.rb
+++ spec/models/kudo_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Kudo, type: :model do
   context 'when the comic exists' do
-    given!(:user) { create(:user) }
-    given!(:comic) { create(:comic) }
+    let!(:comic) { create(:comic) }
 
     it 'can be given' do
       pending 'This model redesign TBD'
       comic.add_kudos(user)
     end
+  end
 end
