fe69a37e086f3812b3f35c7c9d6a5314ac20048b
Tosiaki <aworldwithoutsnowflakes1@gmail.com>
2019-01-04 21:12:29 -0500

    Fixing signal boost migration

--- db/migrate/20181212193632_create_signal_boosts.rb
+++ db/migrate/20181212193632_create_signal_boosts.rb
@@ -1,8 +1,7 @@
 class CreateSignalBoosts < ActiveRecord::Migration[5.2]
   def change
     create_table :signal_boosts do |t|
-      t.references :post, foreign_key: true
-      t.references :origin, foreign_key: true
+      t.references :origin, foreign_key: { to_table: :articles }
 
       t.timestamps
     end
