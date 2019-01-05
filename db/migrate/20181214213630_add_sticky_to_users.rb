75e2286fdc0c5ddb3f4662c2aba85d7eec175f52
Tosiaki <aworldwithoutsnowflakes1@gmail.com>
2019-01-04 21:15:49 -0500

    Fixing migration for stickies

--- db/migrate/20181214213630_add_sticky_to_users.rb
+++ db/migrate/20181214213630_add_sticky_to_users.rb
@@ -1,5 +1,5 @@
 class AddStickyToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_reference :users, :sticky, foreign_key: true
+    add_reference :users, :sticky, foreign_key: { to_table: :articles }
   end
 end
