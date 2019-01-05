7237e14c833954a7ad0f7fc04fae456d5133f8e2
Tosiaki <aworldwithoutsnowflakes1@gmail.com>
2019-01-04 21:19:39 -0500

    Fixing migration for reply_to

--- db/migrate/20181215162708_add_reply_to_to_articles.rb
+++ db/migrate/20181215162708_add_reply_to_to_articles.rb
@@ -1,5 +1,5 @@
 class AddReplyToToArticles < ActiveRecord::Migration[5.2]
   def change
-    add_reference :articles, :reply_to, foreign_key: true
+    add_reference :articles, :reply_to, foreign_key: { to_table: :articles }
   end
 end
