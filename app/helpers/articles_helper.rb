68e845e52d504df277f5197b52d88f8bd38e3f7f
Tosiaki <aworldwithoutsnowflakes1@gmail.com>
2019-07-03 19:13:48 -0400

    Adding font tag and color attribute to the list of whitelisted tags

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -17,8 +17,8 @@ module ArticlesHelper
 
   def sanitize_with_loofah(content)
     scrubber = Rails::Html::PermitScrubber.new
-    scrubber.tags = %w(b i u s strike strong em a ul ol li p br img div span table tr th td figure dd dl dt blockquote h1 h2 h3 h4 h5 h6)
-    scrubber.attributes = %w(href style src class)
+    scrubber.tags = %w(b i u s strike strong em a ul ol li p br img div span table tr th td figure dd dl dt blockquote h1 h2 h3 h4 h5 h6 font)
+    scrubber.attributes = %w(href style src class color)
     unformatted_html = Nokogiri::XML::Node::SaveOptions.class_eval { |m| m::DEFAULT_HTML ^ m::FORMAT }
     Loofah.fragment(content).scrub!(scrubber).to_html(save_with: unformatted_html).html_safe
   end
