85614d5ea7fdf98d4c490be95a5396ddc5724881
Tosiaki <aworldwithoutsnowflakes1@gmail.com>
2019-07-23 14:06:26 -0400

    Adding ruby to allowed tags

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -17,7 +17,7 @@ module ArticlesHelper
 
   def sanitize_with_loofah(content)
     scrubber = Rails::Html::PermitScrubber.new
-    scrubber.tags = %w(b i u s strike strong em a ul ol li p br img div span table tr th td figure dd dl dt blockquote h1 h2 h3 h4 h5 h6 font)
+    scrubber.tags = %w(b i u s strike strong em a ul ol li p br img div span table tr th td figure dd dl dt blockquote h1 h2 h3 h4 h5 h6 font ruby rb rp rt)
     scrubber.attributes = %w(href style src class color)
     unformatted_html = Nokogiri::XML::Node::SaveOptions.class_eval { |m| m::DEFAULT_HTML ^ m::FORMAT }
     Loofah.fragment(content).scrub!(scrubber).to_html(save_with: unformatted_html).html_safe
