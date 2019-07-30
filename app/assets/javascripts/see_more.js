900b26019aea14af413b970c1a053585cc0b933e
Tosiaki <aworldwithoutsnowflakes1@gmail.com>
2019-07-30 13:58:39 -0400

    Adding show hide extendec content

---
+++ app/assets/javascripts/see_more.js
@@ -0,0 +1,27 @@
+$(document).on('turbolinks:load', function(){
+	articleEntries = document.getElementsByClassName('article-content-container');
+	Array.prototype.forEach.call(articleEntries, function(articleEntry){
+		if (articleEntry.scrollHeight > articleEntry.clientHeight) {
+			var seeMoreText = document.createTextNode("See more");
+			var anchor = document.createElement('a');
+			anchor.appendChild(seeMoreText);
+			anchor.href = "#";
+			var seeMoreContainer = document.createElement('div');
+			seeMoreContainer.classList.add('see-more-link');
+			seeMoreContainer.appendChild(anchor);
+			articleEntry.parentNode.insertBefore(seeMoreContainer, articleEntry.nextSibling);
+
+			seeMoreContainer.addEventListener('click', function(event) {
+				event.preventDefault();
+				articleContainer = seeMoreContainer.previousSibling;
+				if (articleContainer.classList.contains("shortened-article")) {
+					articleContainer.classList.remove('shortened-article');
+					seeMoreText.nodeValue = "See less";
+				} else {
+					articleContainer.classList.add('shortened-article');
+					seeMoreText.nodeValue = "See more";
+				}
+			});
+		}
+	});
+});
