b62d780f845c14547affdb7bf562a8e936aa27ce
author <example@test.com>
2020-02-05 21:35:52 -0500

    Fixing redis url

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
-REDIS_GATHERING = Redis::Namespace.new("Gathering", redis: Redis.new)
-REDIS_THINGS = Redis::Namespace.new("Things", redis: Redis.new)
+REDIS_GATHERING = Redis::Namespace.new("Gathering", redis: Redis.new(url: ENV["REDISCLOUD_URL"]))
+REDIS_THINGS = Redis::Namespace.new("Things", redis: Redis.new(url: ENV["REDISCLOUD_URL"]))
