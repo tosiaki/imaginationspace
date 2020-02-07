61bfc53236952245743e60b1222581b3b3d35a6a
author <example@test.com>
2020-02-07 01:49:24 -0500

    Reverting to suckerpunch for now

--- app/channels/user_channel.rb
+++ app/channels/user_channel.rb
@@ -14,7 +14,33 @@ class UserChannel < ApplicationCable::Channel
   end
 
 	def gather(data)
-		GatheringJob.perform_later(current_user.id, data['gathering'])
+		item = Item.find_by(name: data['gathering'])
+		return unless item
+		item_gathering = item.gatherings.first
+		return unless item_gathering
+		action.setnx(current_user.id, 0)
+		action.incr(current_user.id)
+		thing_key = "#{current_user.id}:#{item.name}"
+		things.setnx(thing_key, 0)
+		action_id = action.get(current_user.id)
+		Thread.new do
+			while action_id == action.get(current_user.id)
+				sleep poisson(item_gathering.delay)
+				if action_id == action.get(current_user.id)
+					things.incr(thing_key)
+					self.class.broadcast_to current_user, thing: item.name,
+						amount: things.get(thing_key) 
+				end
+			end
+		end
+	end
+
+	def poisson(delay)
+		-Math.log(1.0 - Random.rand)*delay
+	end
+
+	def action
+		REDIS_ACTION
 	end
 
 	def things
--- app/jobs/gathering_job.rb
+++ app/jobs/gathering_job.rb
@@ -1,29 +1,29 @@
 class GatheringJob < ApplicationJob
   queue_as :default
 
-  def perform(user_id, item)
+  def perform(user_id, item_name)
 		user = User.find(user_id)
-		item = Item.find_by(name: data['gathering'])
+		item = Item.find_by(name: item_name)
 		return unless item
 		item_gathering = item.gatherings.first
 		return unless item_gathering
-		action_id = action.get(user_id)
 		action.setnx(user_id, 0)
 		action.incr(user_id)
-		thing_key = "#{user_id}:#{item.name}"
+		thing_key = "#{user_id}:#{item_name}"
 		things.setnx(thing_key, 0)
+		action_id = action.get(user_id)
 		while action_id == action.get(user_id)
 			sleep poisson(item_gathering.delay)
 			if action_id == action.get(user_id)
 				things.incr(thing_key)
-				UserChannel.broadcast_to user, thing: item.name,
+				UserChannel.broadcast_to user, thing: item_name,
 					amount: things.get(thing_key)
 			end
 		end
   end
 
-	def poisson(rateParameter)
-		-Math.log(1.0 - Random.rand)/rateParameter
+	def poisson(delay)
+		-Math.log(1.0 - Random.rand)*delay
 	end
 
 	def action
