9f665e2f5ac1b1072a25e458d761035fe9086a6c
Tosiaki 「」 <aworldwithoutsnowflakes1@gmail.com>
2021-04-19 13:46:52 -0400

    Moving the content display above.

--- app/javascript/components/DisplayContent.svelte
+++ app/javascript/components/DisplayContent.svelte
@@ -54,7 +54,7 @@
     left: 0;
     position: fixed;
     width: 100%;
-    z-index: 2;
+    z-index: 3;
     background-color: rgba(255, 255, 255, 0.8);
     padding: 0 1em;
   }
@@ -68,7 +68,7 @@
     position: fixed;
     top: 0;
     left: 0;
-    z-index: 1;
+    z-index: 2;
     width: 100%;
     height: 100%;
     background-color: rgba(0, 0, 0, 0.4);
