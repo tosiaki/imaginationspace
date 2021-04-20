ee40b48dbfe01f7b4ca39dad6fb0acafa8a5b6cf
Tosiaki 「」 <aworldwithoutsnowflakes1@gmail.com>
2021-04-19 20:32:25 -0400

    Fixing behavior when pressing back button.

--- app/javascript/packs/svelte_parts.js
+++ app/javascript/packs/svelte_parts.js
@@ -6,42 +6,47 @@ import DisplayContent from '../components/DisplayContent.svelte';
 import { displayContentStore } from '../stores';
 
 document.addEventListener('turbolinks:load', event => {
-  if(!document.body.dataset.svelteLoaded) {
-    new CreateSeriesButton({
-      target: document.getElementById('create-series-button')
-    });
+  const createSeriesButton = document.getElementById('create-series-button');
+  createSeriesButton.innerHTML = '';
+  new CreateSeriesButton({
+    target: createSeriesButton
+  });
 
-    for (let addToSeriesButton of document.getElementsByClassName('add-to-series')) {
-      new AddToSeriesButton({
-        target: addToSeriesButton,
-        props: {
-          articleId: addToSeriesButton.dataset.articleId
-        }
+  for (let addToSeriesButton of document.getElementsByClassName('add-to-series')) {
+    addToSeriesButton.innerHTML = '';
+    new AddToSeriesButton({
+      target: addToSeriesButton,
+      props: {
+        articleId: addToSeriesButton.dataset.articleId
+      }
+    });
+  }
+
+  const modalWindows = document.getElementById('modal-windows');
+  modalWindows.innerHTML = '';
+  new ModalWindows({
+    target: modalWindows
+  });
+
+  const flashMessages = document.getElementById('flash-messages');
+  flashMessages.innerHTML = '';
+  new FlashMessages({
+    target: flashMessages
+  });
+
+  const displayContent = document.getElementById('display-content');
+  displayContent.innerHTML = '';
+  new DisplayContent({
+    target: displayContent
+  });
+
+  for (let statusArea of document.getElementsByClassName('status-content')) {
+    statusArea.addEventListener('click', event => {
+      event.preventDefault();
+      displayContentStore.set({
+        article: statusArea.dataset.articleId,
+        pages: statusArea.dataset.pages
       });
-    }
-
-    new ModalWindows({
-      target: document.getElementById('modal-windows')
     });
-
-    new FlashMessages({
-      target: document.getElementById('flash-messages')
-    });
-
-    new DisplayContent({
-      target: document.getElementById('display-content')
-    });
-
-    for (let statusArea of document.getElementsByClassName('status-content')) {
-      statusArea.addEventListener('click', event => {
-        event.preventDefault();
-        displayContentStore.set({
-          article: statusArea.dataset.articleId,
-          pages: statusArea.dataset.pages
-        });
-      });
-    }
-
-    document.body.dataset.svelteLoaded = true;
   }
 });
