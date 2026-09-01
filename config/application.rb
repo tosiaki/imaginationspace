a7045471d3e2e2f45dcd732a85d0dd1c42668151
Tosiaki 「」 <tosiaki@example.com>
2026-09-01 01:46:42 -0400

    Load Rails 8.1 framework defaults

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ Bundler.require(*Rails.groups)
 module Fancomics
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 8.0
+    config.load_defaults 8.1
     config.active_storage.variant_processor = :mini_magick
 
     # Configuration for the application, engines, and railties goes here.
