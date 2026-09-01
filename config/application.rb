c95fea3d137abe3ae551fdbe7dda714d75440a0a
Tosiaki 「」 <tosiaki@example.com>
2026-09-01 00:47:06 -0400

    Load Rails 7.1 framework defaults

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ Bundler.require(*Rails.groups)
 module Fancomics
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 7.1
     config.active_storage.variant_processor = :mini_magick
 
     # Configuration for the application, engines, and railties goes here.
