1654ddaa01bed634fd9902a80ef52d86df810b39
Tosiaki 「」 <tosiaki@example.com>
2026-09-01 12:00:44 -0400

    Repair redirect-only controller test

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,9 +1,12 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get pages_home_url
-    assert_response :success
+  test "redirects the retired home page without indexing it" do
+    get root_url
+
+    assert_redirected_to "https://discord.gg/e97QGEA"
+    assert_equal "public, max-age=86400", response.headers["Cache-Control"]
+    assert_equal "noindex, nofollow, noarchive, nosnippet", response.headers["X-Robots-Tag"]
   end
 
 end
